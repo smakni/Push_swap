5fd3997896b9ad81101618985be32a6163372f1e
Sabri MAKNI <sabrimakni@gmail.com>
2018-11-27 18:05:14 +0100

    add print_test

--- src/print_pile.c
+++ src/print_pile.c
@@ -6,12 +6,37 @@
 /*   By: smakni <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 15:51:48 by smakni            #+#    #+#             */
-/*   Updated: 2018/11/26 15:34:39 by smakni           ###   ########.fr       */
+/*   Updated: 2018/11/27 18:01:43 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
+void	print_pile(s_pile *tab)
+{
+	int i;
+	static int count = 0;
+
+	i = tab->lt - 1;
+	ft_printf("\n|---------------------|\n");
+	while (i >= 0)
+	{
+		if (i > tab->la - 1)
+			ft_printf("%-11.d|", tab->a[i]);
+		else
+			ft_printf("%-11d|", tab->a[i]);
+		if (i > tab->lb - 1)
+			ft_printf("%11.d\n", tab->b[i]);
+		else	
+			ft_printf("%11d\n", tab->b[i]);
+		i--;
+	}
+	ft_printf("|---------------------|\n");
+	ft_printf("|pile_a         pile_b|\n");
+	ft_printf("|>>>>>>>[%5d]<<<<<<<|\n", count++);
+	ft_printf("|---------------------|\n");
+}
+/*
 void	print_pile(s_pile *tab)
 {
 	int i;
@@ -35,4 +60,4 @@ void	print_pile(s_pile *tab)
 	ft_printf("|pile_a         pile_b|\n");
 	ft_printf("|>>>>>>>[%5d]<<<<<<<|\n", count++);
 	ft_printf("|---------------------|\n");
-}
+}*/
